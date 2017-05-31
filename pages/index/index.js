711a002a84d7cb89bfd5140a1977c7de2892ef63
李文洪 <784327282@qq.com>
2017-05-31 16:26:44 +0800

    add images and write part of index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,14 +3,25 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
   },
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
+  },
+  // 进入找老师界面
+  findTeacher:function(){
+    console.log('findTeacher')
   },
   onLoad: function () {
     console.log('onLoad')
